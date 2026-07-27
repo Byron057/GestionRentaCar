4f23edcc41cabeccefd3cad0e8fbedc4dced0fe6
Byron057 <Byronvelastegui07@gmail.com>
2026-07-27 18:05:15 -0500

    ..

---
+++ src/Conexion/conexion.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+/**
+ *
+ * @author PC
+ */
+public class conexion {
+     private static final String URL = "jdbc:mysql://localhost:3306/gestion_alquiler_autos";
+    private static final String USER = "root";
+    private static final String PASSWORD= "";
+    
+    private Connection conn;
+    public Connection getConnection(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(URL,USER,PASSWORD);
+        }catch(ClassNotFoundException | SQLException e){
+            System.out.println(e);
+        }
+        return conn;
+    }
+    
+}
