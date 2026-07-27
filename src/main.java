97dd15e7c45e0816137e72f3d9f8b25da2e7b951
Byron057 <Byronvelastegui07@gmail.com>
2026-07-27 18:46:54 -0500

    ,,

--- src/main.java
+++ src/main.java
@@ -3,14 +3,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 import Views.frames.Login;
+import Conexion.conexion;
+import java.sql.Connection;
 /**
  *
  * @author PC
  */
 public class main {
      public static void main(String[] args) {
-        Login login = new Login();
-        login.setLocationRelativeTo(null); // Centra la ventana (opcional)
-        login.setVisible(true);
+         
+        conexion conn = new conexion();
+        Connection con = conn.getConnection();
+        if(con !=null){
+            Login login = new Login();
+            login.setLocationRelativeTo(null); // Centra la ventana (opcional)
+            login.setVisible(true);
+            System.out.println("conexion Esitosa");
+        }else{
+            System.out.println("Error en la conexion");
+        }
+        
     }
 }
