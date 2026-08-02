8f6b0021d1d49782037a863a8ce4e5e27b2d2db2
Nico <nicojorge666@gmail.com>
2026-08-02 13:59:14 -0500

    listarDAO

--- src/DAO/alquiledesDAO.java
+++ src/DAO/alquiledesDAO.java
@@ -9,6 +9,7 @@ import Models.alquileres;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,12 +40,48 @@ public class alquiledesDAO {
 
             return true;
 
-        } catch (Exception e) {
+        } catch (SQLException e) {
 
             System.out.println("Error: " + e.toString());
             return false;
 
         }
     }
-    
+     // LISTAR
+    public List<alquileres> listarAlquileres() {
+
+        List<alquileres> lista = new ArrayList<>();
+
+        String sql = "SELECT * FROM alquileres";
+
+        try {
+
+            con = cn.getConnection();
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+
+                alquileres a = new alquileres();
+
+                a.setIdAlquiler(rs.getInt("id_alquiler"));
+                a.setFkIdCliente(rs.getInt("fk_id_cliente"));
+                a.setFkIdVehiculo(rs.getInt("fk_id_vehiculo"));
+                a.setFechaAlquiler(rs.getString("fecha_alquiler"));
+                a.setTotal(rs.getDouble("total"));
+                a.setEstado(rs.getString("estado"));
+                a.setDias(rs.getInt("dias"));
+
+                lista.add(a);
+
+            }
+
+        } catch (SQLException e) {
+
+            System.out.println("Error: " + e.toString());
+
+        }
+
+        return lista;
+    }
 }
