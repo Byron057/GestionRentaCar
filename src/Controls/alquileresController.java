8368ac8a3c71a6843c75844a1230c621e427e68b
Nico <nicojorge666@gmail.com>
2026-08-02 14:04:02 -0500

    contruccionController

--- src/Controls/alquileresController.java
+++ src/Controls/alquileresController.java
@@ -4,10 +4,18 @@
  */
 package Controls;
 
-/**
- *
- * @author Nikholov
- */
+import DAO.alquileresDAO;
+import Models.alquileres;
+import Views.panels.RentasPanel;
+import javax.swing.JOptionPane;
 public class alquileresController {
-    
+    private RentasPanel vista;
+    private alquileresDAO dao;
+
+    public alquileresController(RentasPanel vista){
+
+        this.vista=vista;
+        dao=new alquileresDAO();
+
+    }
 }
--- src/DAO/alquileresDAO.java
+++ src/DAO/alquileresDAO.java
@@ -13,7 +13,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class alquiledesDAO {
+public class alquileresDAO {
     conexion cn = new conexion();
     Connection con;
     PreparedStatement ps;
